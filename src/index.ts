9118b9a59abb13940d0b0664715a89cf4564676f
YEAHx4 <45203447+5tarlight@users.noreply.github.com>
2021-01-03 10:22:25 +0900

    negative check

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ export class IAException extends InvalidArgumentException {}
 export class RException extends RangeException {}
 
 export const isOdd = (value: number) => {
+  if (value < 0)
+     throw new IAException('negative quantity is not allowed');
+  
   if (!Number.isInteger(value))
     throw new IAException('expected an integer');
   
