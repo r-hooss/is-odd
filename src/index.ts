224202414cfd8751847bab97d27f0cafc0130576
YEAHx4 <45203447+5tarlight@users.noreply.github.com>
2021-01-03 10:58:14 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import { InvalidArgumentException, RangeException } from 'node-exceptions';
-
-export class IAException extends InvalidArgumentException {}
-export class RException extends RangeException {}
-
 export const isOdd = (value: number) => {
   if (value < 0)
-     throw new IAException('negative quantity is not allowed');
+     throw new Error('negative quantity is not allowed');
   
   if (!Number.isInteger(value))
-    throw new IAException('expected an integer');
+    throw new Error('expected an integer');
   
   return value % 2 === 1;
 }
